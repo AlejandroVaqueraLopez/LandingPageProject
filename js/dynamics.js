92e9aeb9805bac045c62f4a993d7d915a75e5d11
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2025-08-12 17:09:13 -0700

    feat: v1.0.0

--- js/dynamics.js
+++ js/dynamics.js
@@ -10,7 +10,7 @@ let transitionElementArray = document.querySelectorAll(".transitionElement");
 let transitionReverseElementArray = document.querySelectorAll(".transitionReverseElement");
 let footerElementArray = document.querySelectorAll(".footerElementArray");
 //button that activates layout
-let btnActivateLayout = document.getElementById("btnActivateLayout");
+
 let btnGoUp = document.getElementById("btnGoUp");
 //initialize the translateX of elements
 setPosition(transitionElementArray);
@@ -71,23 +71,7 @@ function setPositionReverse(transitionReverseElementArray){
 	cont = 0;
 }
 
-btnActivateLayout.addEventListener("click",()=>{
-	layoutToggle = !layoutToggle;//true or false;
-	cont = 0;
-	if(layoutToggle == true){
-		btnActivateLayout.value = "ACTIVATED";
-		setOutline(headerElementArray,true);
-		setOutline(transitionElementArray,true);
-		setOutline(transitionReverseElementArray,true);
-		setOutline(footerElementArray,true);
-	}else if(layoutToggle == false){
-		btnActivateLayout.value = "ACTIVATE";
-		setOutline(headerElementArray,false);
-		setOutline(transitionElementArray,false);
-		setOutline(transitionReverseElementArray,false);
-		setOutline(footerElementArray,false);
-	}
-});
+
 //this function sets the outline depending of the action value
 function setOutline(element,action){
 	cont = 0;
